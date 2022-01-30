e7419347a25166bce177728ca066f9f1bd663e6e
shecodesincolors <68675700+shecodesincolors@users.noreply.github.com>
2022-01-30 17:21:51 -0600

    Ejercicios Resueltos

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+/*
+EJERCICIOS JS 
+Carolina Aide Ramírez Lara
+ada_hack275
+*/
+
+//EJERICIO 1
+
 const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
 ];
+
+function esPrimo(num){
+  for(let i=2; num>i; i++){
+    if(num%i==0){
+      return false;
+    }else{
+  return num>1;}
+}}
+
+console.log("Ejercicio 1");
+let Primos=ejercicio1.filter(esPrimo);
+console.log(`Estos numeros son primos:${Primos}`);
+
+
+//console.log(ejercicio1.filter(esPrimo));
+
 
 const ejercicio2 = [
   {
@@ -35,3 +59,31 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+//EJERCICIO 2
+console.log("Ejercicio 2");
+function filtroVIP(persona){
+if(persona.edad>=18 && persona.esFamiliar){
+console.log(`Puedes pasar ${persona.nombre}`);
+}}
+
+ejercicio2.forEach((persona)=>{
+filtroVIP(persona);
+});
+
+//EJERCICIO 3
+console.log("Ejercicio 3");
+function fibonacciNumbers(){
+let fib = []; // El array fibonacci
+
+console.log(fib[0] = 0);
+console.log(fib[1] = 1);
+
+for (i = 2; i <= 49; i++) {
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
+console.log(`Se imprimieron los primeros ${fib.length} números de la serie Fibonacci`);
+}
+
+fibonacciNumbers();
